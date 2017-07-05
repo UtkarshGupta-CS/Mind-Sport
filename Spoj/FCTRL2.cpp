c07f9621ad7ef8f77041e1a30f47f8324e287928
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-07-05 21:04:53 +0530

    final newline added

--- Spoj/FCTRL2.cpp
+++ Spoj/FCTRL2.cpp
@@ -5,34 +5,34 @@ using namespace std;
 
 int main()
 {
-   int t;
-   cin >> t;
-   while (t--)
-   {
-      int n, j, temp;
-      cin >> n;
-      int arr[200];
-      arr[0] = 1;
-      j = 0;
-      for (; n >= 2; n--)
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, j, temp;
+    cin >> n;
+    int arr[200];
+    arr[0] = 1;
+    j = 0;
+    for (; n >= 2; n--)
+    {
+      temp = 0;
+      for (int i = 0; i <= j; i++)
       {
-         temp = 0;
-         for (int i = 0; i <= j; i++)
-         {
-            temp = (arr[i] * n) + temp;
-            arr[i] = temp % 10;
-            temp = temp / 10;
-         }
-         while (temp > 0)
-         {
-            arr[++j] = temp % 10;
-            temp = temp / 10;
-         }
+        temp = (arr[i] * n) + temp;
+        arr[i] = temp % 10;
+        temp = temp / 10;
       }
-      for (int i = j; i >= 0; i--)
+      while (temp > 0)
       {
-         printf("%d", arr[i]);
+        arr[++j] = temp % 10;
+        temp = temp / 10;
       }
-      printf("\n");
-   }
+    }
+    for (int i = j; i >= 0; i--)
+    {
+      printf("%d", arr[i]);
+    }
+    printf("\n");
+  }
 }
